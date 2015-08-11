69e26c28470df718d455f8098f88aa6d5b9c7ed1
itai-m <Itaimichael@gmail.com>
2015-08-11 23:02:30 +0300

    add function to some spinners

--- Ex1/src/Ball.java
+++ Ex1/src/Ball.java
@@ -6,45 +6,52 @@ public class Ball {
 	private double v;
 	private double g;
 	private boolean up;
-	private int time;
+	private double cor;
 	
 	public Ball(){
 		x = 0;
 		y = 0;
 		size = 10;
 		up = false;
-		time = 0;
+		v = 0;
+		g = 0;
+		cor = 0;
 	}
 	
-	public Ball(double _x, double _y, double _size, double vel, double gra){
+	public Ball(double _x, double _y, double _size, double vel, double gra, double _cor){
 		x = _x;
 		y = _y;
 		size = _size;
 		v = vel;
 		g = gra;
 		up = false;
-		time = 0;
+		cor = _cor;
 	}
 	
 	public void step(){
-		time++;
-		double speed = 0;
-		if (y - size < 0){
-			up = true;
-		}
 		if (up){
-			y = y + (v + (g)/2);
+			v = v - g/2;
+			y = y + v;
 		}
 		else{
-			y = y - (v + (g)/2);
+			v = v + g/2;
+			y = y - v;
+		}
+		if (y - size < 0){
+			up = true;
+			y = size;
+		}
+		if (v < 0){
+			up = false;
 		}
 		
 	}
 	
-	public void changeInv(double vel, double gra){
-		v = vel;
+	public void changeInv(double _cor, double gra){
+		cor = _cor;
 		g = gra;
 	}
+	
 	
 	public double getX(){
 		return x;
@@ -52,6 +59,11 @@ public class Ball {
 	
 	public double getY(){
 		return y;
+	}
+	
+	public void setY(int _y){ 
+		up = false;
+		y = _y;
 	}
 	
 	public double getSize(){
--- Ex1/src/MainPanel.java
+++ Ex1/src/MainPanel.java
@@ -18,17 +18,26 @@ import javax.swing.Timer;
 
 
 public class MainPanel extends JPanel implements ActionListener{
+	private final int ballSize = 40;
+	private final String Path = "C:\\Users\\Itai\\workspace\\Games-in-Java\\Ex1\\src\\";
+	private final double defaultGravConst = 9.8;
+	private final double earthGravConst = 9.8;
+	private final double moonGravConst = 1.6;
+	private final double marsGravConst = 3.8;
+	private final double otherGravConst = 5;
+	private final double defaultCOR = 0.8;
+	
 	private JComboBox<String> wallcombo;
 	private JButton cmdStart, cmdReset;
 	private JLabel lblEnvironment, lblGrev, lblEla, lblIni;
 	private JSpinner spinGrav, spinEla, spinIni;
 	private String trans = "";
 	private BallPanel t;
-	private String Path = "C:\\Users\\Itai\\workspace\\Games-in-Java\\Ex1\\src\\";
-	SpinnerModel  spinnerModel = new SpinnerNumberModel(5, 1, 50, 1);
+	private SpinnerModel  graSpinnerModel = new SpinnerNumberModel(9.8, 0, 50, 0.2);
+	private SpinnerModel  elaSpinnerModel = new SpinnerNumberModel(0.8, 0, 1, 0.1);
+	private SpinnerModel  iniSpinnerModel = new SpinnerNumberModel(50, 1, 500, 1);
 	private Timer timer;
 	private Ball ball;
-	private int ballSize = 40;
 	
 	
 	
@@ -37,21 +46,40 @@ public class MainPanel extends JPanel implements ActionListener{
 		String[] s = {"Default", "Earth", "Moon", "Mars", "Other"};
 		wallcombo = new JComboBox<>(s);
 		lblGrev = new JLabel("Gravitational constant:");
-		spinGrav = new JSpinner(spinnerModel);
+		spinGrav = new JSpinner(graSpinnerModel);
 		lblEla = new JLabel("Elastic constant:");
-		spinEla = new JSpinner(spinnerModel);
+		spinEla = new JSpinner(elaSpinnerModel);
 		lblIni = new JLabel("Initialized hight:");
-		spinIni = new JSpinner(spinnerModel);
+		spinIni = new JSpinner(iniSpinnerModel);
 		cmdReset = new JButton("Reset");
 		cmdStart = new JButton("Start");
-		timer = new Timer(100,this);
+		timer = new Timer(20,this);
 		timer.start();
-		ball = new Ball(getWidth()/2, 500, ballSize, 1, 9.8);
+		ball = new Ball(200, 500, ballSize, 0, defaultGravConst, defaultCOR);
 		System.out.println("Height: " +getHeight());
 		wallcombo.addActionListener(new ActionListener() {
 			@Override
             public void actionPerformed(ActionEvent e) {
                 trans = (String)wallcombo.getSelectedItem();
+                if (trans.equals("Earth")){
+                	spinGrav.setValue(earthGravConst);
+                	ball.changeInv((double)spinEla.getValue(), (double)spinGrav.getValue());
+    	        }
+    	        else if (trans.equals("Moon")){
+    	        	spinGrav.setValue(moonGravConst);
+    	        	ball.changeInv((double)spinEla.getValue(), (double)spinGrav.getValue());
+    	        }
+    	        else if (trans.equals("Mars")){
+    	        	spinGrav.setValue(marsGravConst);
+    	        	ball.changeInv((double)spinEla.getValue(), (double)spinGrav.getValue());
+    	        }
+    	        else if (trans.equals("Other")){
+    	        	spinGrav.setValue(otherGravConst);
+    	        	ball.changeInv((double)spinEla.getValue(), (double)spinGrav.getValue());
+    	        }
+    	        else{
+    		        
+    	        }
                 t.repaint();
             }
         });
@@ -59,10 +87,21 @@ public class MainPanel extends JPanel implements ActionListener{
 		cmdReset.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                trans = (String)wallcombo.getSelectedItem();
+            	ball.setY((int)spinIni.getValue());
+            	timer.stop();
                 t.repaint();
             }
         });
+		
+		cmdStart.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            	ball.changeInv((double)spinEla.getValue(), (double)spinGrav.getValue());
+            	timer.start();
+                t.repaint();
+            }
+        });
+		
 		
         setLayout(new BorderLayout());
         JPanel pNorth = new JPanel();
@@ -116,8 +155,6 @@ public class MainPanel extends JPanel implements ActionListener{
 	public void actionPerformed(ActionEvent e) {
 		repaint();
 		ball.step();
-		System.out.println(ball.getY());
-		
 	}
 }
 
