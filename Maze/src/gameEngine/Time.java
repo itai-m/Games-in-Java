90785905238425ebea44836a5149d43b53e2463e
itai-m <Itaimichael@gmail.com>
2015-08-25 23:51:07 +0300

    add comment to the Time, Vector2f and vector3f

--- Maze/src/gameEngine/Time.java
+++ Maze/src/gameEngine/Time.java
@@ -2,17 +2,20 @@ package gameEngine;
 
 public class Time {
 
-	public static final long SECOND = 1000000000L;
-	private static double delta;
+	public static final long SECOND = 1000000000L;  ///Save one second
+	private static double delta;  ///Save the delta between times
 	
+	///Return the current time
 	public static long getTime(){
 		return System.nanoTime();
 	}
 	
+	///Get the delta
 	public static double getDelta(){
 		return delta;
 	}
 	
+	///Set the delta
 	public static void setDelta(double _delta){
 		delta = _delta;
 	}
--- Maze/src/gameEngine/Vector3f.java
+++ Maze/src/gameEngine/Vector3f.java
@@ -6,20 +6,24 @@ public class Vector3f {
 	private float y;
 	private float z;
 	
+	///Constructor
 	public Vector3f(float x, float y, float z){
 		this.x = x;
 		this.y = y;
 		this.z = z;
 	}
 	
+	///Return the length of the vector
 	public float lenght(){
 		return (float)Math.sqrt(x * x + y * y + z * z);
 	}
 	
+	///Return the number of dot action result
 	public float dot(Vector3f v){
 		return (x * v.getX() + y * v.getY() + z * v.getZ());
 	}
 	
+	///Return the vector after cross action result with another vector
 	public Vector3f cross(Vector3f v){
 		float x_ = y * v.getZ() - z * v.getY();
 		float y_ = z * v.getX() - x * v.getZ();
@@ -27,6 +31,7 @@ public class Vector3f {
 		return new Vector3f(x_, y_, z_);
 	}
 	
+	///Normalize and return the vector
 	public Vector3f normalize(){
 		float lenght = lenght();
 		x /= lenght;
@@ -35,62 +40,77 @@ public class Vector3f {
 		return this;
 	}
 	
+	///Return the result of adding this vector with another vector
 	public Vector3f add(Vector3f v){
 		return new Vector3f(x + v.getX(), y + v.getY(), z + v.getZ());
 	}
 	
+	///Return the result adding of this vector with number
 	public Vector3f add(float num){
 		return new Vector3f(x + num, y + num, z + num);
 	}
 
+	///Return the result of subtracting this vector with another vector
 	public Vector3f sub(Vector3f v){
 		return new Vector3f(x - v.getX(), y - v.getY(), z - v.getZ());
 	}
 	
+	///Return the result of subtracting this vector with number
 	public Vector3f sub(float num){
 		return new Vector3f(x - num, y - num, z - num);
 	}
 	
+	///Return the result of multiply this vector with another vector
 	public Vector3f mul(Vector3f v){
 		return new Vector3f(x * v.getX(), y * v.getY(), z * v.getZ());
 	}
 	
+	///Return the result of multiply this vector with number
 	public Vector3f mul(float num){
 		return new Vector3f(x * num, y * num, z * num);
 	}
 	
+	///Return the result of dividing this vector with number
 	public Vector3f div(Vector3f v){
 		return new Vector3f(x / v.getX(), y / v.getY(), z / v.getZ());
 	}
 	
+	///Return the result of dividing this vector with number
 	public Vector3f div(float num){
 		return new Vector3f(x / num, y / num, z / num);
 	}
 	
+	///Return the string with the number of the vector
 	public String toString(){
 		return ("(" + x + "," + y + "," + z + ")");
 	}
 
+	///Get the x
 	public float getX() {
 		return x;
 	}
 
+	///Set the x
 	public void setX(float x) {
 		this.x = x;
 	}
 
+	///Get the y
 	public float getY() {
 		return y;
 	}
 
+	///Set the y
 	public void setY(float y) {
 		this.y = y;
 	}
 
+	///Get the z
 	public float getZ() {
 		return z;
 	}
 
+	///Set the z
 	public void setZ(float z) {
 		this.z = z;
 	}
