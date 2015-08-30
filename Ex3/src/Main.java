439b58d6ca48a3bf9a4cc1bbef131b6a5f90edcb
itai-m <Itaimichael@gmail.com>
2015-08-30 16:45:54 +0300

    add the main to ex3

---
+++ Ex3/src/Main.java
@@ -0,0 +1,27 @@
+
+import javax.swing.JFrame;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author user
+ */
+public class Main {
+	private final static int initializedHight = 400;
+	private final static int initializedWidth = 400;
+	
+    public static void main(String[] args)
+    {
+        JFrame frame = new JFrame("Ex3");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(initializedWidth, initializedHight);
+        MainPanel p = new MainPanel();
+        frame.add(p);
+        frame.setVisible(true);
+        
+    }
+}
---
+++ Ex3/src/MainPanel.java
@@ -0,0 +1,14 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JPanel;
+
+public class MainPanel extends JPanel implements ActionListener {
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
