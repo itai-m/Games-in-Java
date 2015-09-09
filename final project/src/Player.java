8270545957a4d38d3dfbd65688239082c1ae35f3
MintBerry <itaimichael@gmail.com>
2015-09-09 08:08:32 +0300

    add the turn option to the player class

--- final project/src/Player.java
+++ final project/src/Player.java
@@ -1,25 +1,62 @@
 
 public class Player extends Sprite{
 	
-	private int x;
-	private int y;
+	private final int SPEED = 4;
+	private final int LEFT = 1;
+	private final int RIGHT = 2;
+	private final int UP = 3;
+	private final int DOWN = 4;
+
 	private int height;
 	private int width;
+	private int turnTo;
 	
 	///Default constructor
 	public Player (){
-		x = 0;
-		y = 0;
+		super();
+		turnTo = RIGHT;
 		height = 10;
 		width = 10;
 	}
 	
 	///Constructor
-	public Player(int x, int y, int width, int height){
-		this.x = x;
-		this.y = y;
+	public Player(int x, int y, int width, int height, int boardWidth, int boardHeight){
+		super(x, y, 0, 1, SPEED, boardWidth, boardHeight);
 		this.height = height;
 		this.width = width;
+		turnTo = RIGHT;
+	}
+
+	///Turn to the left, return false if is allready turn to the left, otherwise true
+	public boolean turnLeft(){
+		super.setDirection(new Vector2f (0, -1));
+		return turnToSide(LEFT);
+	}
+
+	///Turn to the right, return false if is allready turn to the right, otherwise true
+	public boolean turnRight(){
+		super.setDirection(new Vector2f (0, 1));
+		return turnToSide(RIGHT);
+	}
+
+	///Turn to the up, return false if is allready turn to the up, otherwise true
+	public boolean turnUp(){
+		super.setDirection(new Vector2f (-1, 0));
+		return turnToSide(UP);
+	}
+
+	///Turn to the down, return false if is allready turn to the down, otherwise true
+	public boolean turnDown(){
+		super.setDirection(new Vector2f (1, 0));
+		return turnToSide(DOWN);
+	}
+
+	///Turn to side, if allready on that side return false, otherwise true
+	private boolean turnToSide(int side){
+		if (turnTo == side)
+			return false;
+		turnTo = side;
+		return true;
 	}
 
 	///Get the x
